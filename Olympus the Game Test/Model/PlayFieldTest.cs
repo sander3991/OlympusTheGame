13ef345e8e532b65cd2829f25476a7c8d53defbc
Ruben Oost <mail@rubenoost.nl>
2014-09-17 20:10:15 +0200

    Add unit test project and included unit test for PlayField.

---
+++ Olympus the Game Test/Model/PlayFieldTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OlympusTheGame;
+
+namespace Olympus_the_Game_Test.Model
+{
+    [TestClass]
+    public class PlayFieldTest
+    {
+        [TestMethod]
+        public void TestAddGameObjects()
+        {
+            // Allocate
+            PlayField pf = new PlayField();
+            GameObject g1 = new ObjectFinish();
+            GameObject g2 = new ObjectStart();
+
+            // Act / Assert
+            pf.AddObject(g1); // Add finish
+            Assert.AreEqual(pf.GetObjects().Count, 1);
+            Assert.AreEqual(pf.GetObjects()[0], g1);
+
+            // Act / Assert
+            try
+            {
+                pf.AddObject(g1); // Add another finish, should throw error
+                Assert.Fail("It is possible to add another finish");
+            }
+            catch (Exception) { } // TODO Make exception more specific
+
+
+            // Act / Assert
+            pf.AddObject(g2); // Add start
+            Assert.AreEqual(pf.GetObjects().Count, 2);
+            Assert.IsTrue((pf.GetObjects()[0] == g1 && pf.GetObjects()[1] == g2) ^ (pf.GetObjects()[0] == g2 && pf.GetObjects()[1] == g1));
+        }
+    }
+}
