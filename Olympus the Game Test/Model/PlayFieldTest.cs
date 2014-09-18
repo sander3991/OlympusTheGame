86eeff384d9c7f03acb7f5a71a31d888b4418fb8
Ruben Oost <mail@rubenoost.nl>
2014-09-18 09:37:05 +0200

    Buffed out unit test for PlayField.addObjects() and PlayField.getObjects()

--- Olympus the Game Test/Model/PlayFieldTest.cs
+++ Olympus the Game Test/Model/PlayFieldTest.cs
@@ -12,26 +12,75 @@ namespace Olympus_the_Game_Test.Model
         {
             // Allocate
             PlayField pf = new PlayField();
-            GameObject g1 = new ObjectFinish();
-            GameObject g2 = new ObjectStart();
+            PlayField pfWithStart = new PlayField();
+            PlayField pfWithFinish = new PlayField();
+            PlayField pfWithStartAndFinish = new PlayField();
+            PlayField pfWith2StartAnd1Finish = new PlayField();
+            PlayField pfWith1StartAnd2Finish = new PlayField();
+
+            GameObject gS = new ObjectFinish();
+            GameObject gF = new ObjectStart();
+            GameObject gS2 = new ObjectFinish();
+            GameObject gF2 = new ObjectStart();
+
+            // Act
+            pfWithStart.AddObject(gS);
+
+            pfWithFinish.AddObject(gF);
+
+            pfWithStartAndFinish.AddObject(gS);
+            pfWithStartAndFinish.AddObject(gF);
+
+            pfWith1StartAnd2Finish.AddObject(gS);
+            pfWith1StartAnd2Finish.AddObject(gF);
+
+            pfWith2StartAnd1Finish.AddObject(gS);
+            pfWith2StartAnd1Finish.AddObject(gF);
 
             // Act / Assert
-            pf.AddObject(g1); // Add finish
-            Assert.AreEqual(pf.GetObjects().Count, 1);
-            Assert.AreEqual(pf.GetObjects()[0], g1);
 
-            // Act / Assert
-            pf.AddObject(g2); // Add start
-            Assert.AreEqual(pf.GetObjects().Count, 2);
-            Assert.IsTrue((pf.GetObjects()[0] == g1 && pf.GetObjects()[1] == g2) ^ (pf.GetObjects()[0] == g2 && pf.GetObjects()[1] == g1));
-
-            // Act / Assert
+            // Add double finish
             try
             {
-                pf.AddObject(g1); // Add another finish, should throw error
+                pfWith1StartAnd2Finish.AddObject(gF2); // should throw error
                 Assert.Fail("It is possible to add another finish");
             }
-            catch (ArgumentException) { } // TODO Make exception more specific
+            catch (ArgumentException) // TODO Make exception more specific
+            {
+                // Should not be added, and length should stay same
+                Assert.AreEqual(2, pfWith1StartAnd2Finish.GetObjects().Count);
+                Assert.IsFalse(pfWith1StartAnd2Finish.GetObjects().Contains(gF2));
+            }
+
+            // Add double start
+            try
+            {
+                pfWith2StartAnd1Finish.AddObject(gS2); // should throw error
+                Assert.Fail("It is possible to add another start");
+            }
+            catch (ArgumentException) // TODO Make exception more specific
+            {
+                // Should not be added, and length should stay same
+                Assert.AreEqual(2, pfWith2StartAnd1Finish.GetObjects().Count);
+                Assert.IsFalse(pfWith2StartAnd1Finish.GetObjects().Contains(gS2));
+            }
+
+            // Assert
+            pfWithStart.AddObject(gS); // Add start
+            Assert.AreEqual(pfWithStart.GetObjects().Count, 1);
+            Assert.AreEqual(pfWithStart.GetObjects()[0], gS);
+
+            // Assert
+            pfWithFinish.AddObject(gS); // Add finish
+            Assert.AreEqual(1, pfWithFinish.GetObjects().Count);
+            Assert.AreEqual(gS, pfWithFinish.GetObjects()[0]);
+
+            // Assert
+            Assert.AreEqual(0, pf.GetObjects().Count);
+
+            // Act / Assert
+            Assert.AreEqual(2, pfWithStartAndFinish.GetObjects().Count);
+            Assert.IsTrue(pfWithStartAndFinish.GetObjects().Contains(gS) && pfWithStartAndFinish.GetObjects().Contains(gF));
         }
     }
 }
