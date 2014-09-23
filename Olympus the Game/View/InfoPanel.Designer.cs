f378faa48b6d00c87dc1dbf82a514f29e5f25413
Martijn <martijnie96@gmail.com>
2014-09-23 13:37:00 +0200

    InfoPanel stijl aangemaakt

--- Olympus the Game/View/InfoPanel.Designer.cs
+++ Olympus the Game/View/InfoPanel.Designer.cs
@@ -28,10 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.StatistiekenTab = new System.Windows.Forms.TabControl();
+            this.Statistieken = new System.Windows.Forms.TabPage();
+            this.StatistiekenTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StatistiekenTab
+            // 
+            this.StatistiekenTab.Controls.Add(this.Statistieken);
+            this.StatistiekenTab.Location = new System.Drawing.Point(3, 3);
+            this.StatistiekenTab.Name = "StatistiekenTab";
+            this.StatistiekenTab.SelectedIndex = 0;
+            this.StatistiekenTab.Size = new System.Drawing.Size(365, 550);
+            this.StatistiekenTab.TabIndex = 0;
+            // 
+            // Statistieken
+            // 
+            this.Statistieken.Location = new System.Drawing.Point(4, 25);
+            this.Statistieken.Name = "Statistieken";
+            this.Statistieken.Padding = new System.Windows.Forms.Padding(3);
+            this.Statistieken.Size = new System.Drawing.Size(357, 521);
+            this.Statistieken.TabIndex = 0;
+            this.Statistieken.Text = "tabPage1";
+            this.Statistieken.UseVisualStyleBackColor = true;
+            // 
+            // InfoPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.StatistiekenTab);
+            this.Name = "InfoPanel";
+            this.Size = new System.Drawing.Size(368, 556);
+            this.StatistiekenTab.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl StatistiekenTab;
+        private System.Windows.Forms.TabPage Statistieken;
     }
 }
