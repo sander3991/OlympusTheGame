6510edd57f300f7c1185781d1f83193040241a80
Ruben Oost <mail@rubenoost.nl>
2014-09-23 11:26:23 +0200

    Basis tekening toegevoegd aan het GamePanel

--- Olympus the Game/View/GamePanel.Designer.cs
+++ Olympus the Game/View/GamePanel.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // GamePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "GamePanel";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintPanel);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
