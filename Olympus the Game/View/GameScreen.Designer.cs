d982c7a6d00679669a8ce816594476165602cd00
Ruben Oost <mail@rubenoost.nl>
2014-09-23 11:31:16 +0200

    GamePanel toegevoegd, deze is verwijderd door merge

--- Olympus the Game/View/GameScreen.Designer.cs
+++ Olympus the Game/View/GameScreen.Designer.cs
@@ -32,6 +32,7 @@
             this.MenuTab = new System.Windows.Forms.TabPage();
             this.InformationTab = new System.Windows.Forms.TabPage();
             this.SettingsTab = new System.Windows.Forms.TabPage();
+            this.gamePanel1 = new Olympus_the_Game.View.GamePanel();
             this.Menu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,18 +41,20 @@
             this.Menu.Controls.Add(this.MenuTab);
             this.Menu.Controls.Add(this.InformationTab);
             this.Menu.Controls.Add(this.SettingsTab);
-            this.Menu.Location = new System.Drawing.Point(12, 464);
+            this.Menu.Location = new System.Drawing.Point(9, 377);
+            this.Menu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Menu.Name = "Menu";
             this.Menu.SelectedIndex = 0;
-            this.Menu.Size = new System.Drawing.Size(410, 291);
+            this.Menu.Size = new System.Drawing.Size(308, 236);
             this.Menu.TabIndex = 0;
             // 
             // MenuTab
             // 
-            this.MenuTab.Location = new System.Drawing.Point(4, 25);
+            this.MenuTab.Location = new System.Drawing.Point(4, 22);
+            this.MenuTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MenuTab.Name = "MenuTab";
-            this.MenuTab.Padding = new System.Windows.Forms.Padding(3);
-            this.MenuTab.Size = new System.Drawing.Size(402, 262);
+            this.MenuTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MenuTab.Size = new System.Drawing.Size(300, 210);
             this.MenuTab.TabIndex = 0;
             this.MenuTab.Text = "Menu";
             this.MenuTab.UseVisualStyleBackColor = true;
@@ -59,10 +62,11 @@
             // 
             // InformationTab
             // 
-            this.InformationTab.Location = new System.Drawing.Point(4, 25);
+            this.InformationTab.Location = new System.Drawing.Point(4, 22);
+            this.InformationTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.InformationTab.Name = "InformationTab";
-            this.InformationTab.Padding = new System.Windows.Forms.Padding(3);
-            this.InformationTab.Size = new System.Drawing.Size(402, 262);
+            this.InformationTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.InformationTab.Size = new System.Drawing.Size(300, 210);
             this.InformationTab.TabIndex = 1;
             this.InformationTab.Text = "Information";
             this.InformationTab.UseVisualStyleBackColor = true;
@@ -70,22 +74,31 @@
             // 
             // SettingsTab
             // 
-            this.SettingsTab.Location = new System.Drawing.Point(4, 25);
+            this.SettingsTab.Location = new System.Drawing.Point(4, 22);
+            this.SettingsTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SettingsTab.Name = "SettingsTab";
-            this.SettingsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.SettingsTab.Size = new System.Drawing.Size(402, 262);
+            this.SettingsTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SettingsTab.Size = new System.Drawing.Size(300, 210);
             this.SettingsTab.TabIndex = 2;
             this.SettingsTab.Text = "Settings";
             this.SettingsTab.UseVisualStyleBackColor = true;
             this.SettingsTab.Click += new System.EventHandler(this.SettingsTab_Click);
             // 
+            // gamePanel1
+            // 
+            this.gamePanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gamePanel1.Location = new System.Drawing.Point(13, 12);
+            this.gamePanel1.Name = "gamePanel1";
+            this.gamePanel1.Size = new System.Drawing.Size(1031, 336);
+            this.gamePanel1.TabIndex = 1;
+            // 
             // GameScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1437, 767);
+            this.ClientSize = new System.Drawing.Size(1078, 623);
+            this.Controls.Add(this.gamePanel1);
             this.Controls.Add(this.Menu);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "GameScreen";
             this.Text = "Olympus the Game";
@@ -101,5 +114,6 @@
         private System.Windows.Forms.TabPage MenuTab;
         private System.Windows.Forms.TabPage InformationTab;
         private System.Windows.Forms.TabPage SettingsTab;
+        private GamePanel gamePanel1;
     }
 }
