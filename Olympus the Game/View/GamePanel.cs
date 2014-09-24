d9ee3f72637ebd2844ba27d9db3f952fbd91efb9
Ruben Oost <mail@rubenoost.nl>
2014-09-24 10:56:39 +0200

    GamePanel bijgewerkt, er zijn nu plaatjes, code voor schaal is ingevoerd en een rand om alle plaatjes.

--- Olympus the Game/View/GamePanel.cs
+++ Olympus the Game/View/GamePanel.cs
@@ -11,12 +11,17 @@ namespace Olympus_the_Game.View
 {
     public partial class GamePanel : UserControl
     {
-        private static int BLOCK_SIZE = 50;
-        private static float SCALE = 1.0f;
+        /// <summary>
+        /// Schaal van het speelveld
+        /// </summary>
+        private float SCALE = 1.0f;
+
         /// <summary>
         /// Het speelveld dat moet worden getekend.
         /// </summary>
         private PlayField pf;
+
+        private Dictionary<Type, Bitmap> ImageList = new Dictionary<Type, Bitmap>();
 
         /// <summary>
         /// Maak een nieuw GamePanel aan, deze krijgt als argument het model mee welke moet worden getekend.
@@ -26,13 +31,19 @@ namespace Olympus_the_Game.View
             // Save vars
             this.pf = OlympusTheGame.INSTANCE.pf;
             this.pf.AddObject(new ObjectStart(50, 50, 0, 0));
-            this.pf.AddObject(new ObjectFinish(50, 50, 800, 300));
+            this.pf.AddObject(new ObjectFinish(150, 150, 800, 300));
             this.pf.AddObject(new ObjectObstacle(50, 50, 60, 0));
             this.pf.AddObject(new EntityCreeper(50, 50, 150, 60, 1.0f));
             this.pf.AddObject(new EntityExplode(50, 50, 150, 0, 1.0f));
             this.pf.AddObject(new EntityPlayer(50, 50, 0, 0));
             this.pf.AddObject(new EntitySlower(50, 50, 200, 150));
             this.pf.AddObject(new EntityTimeBomb(50, 50, 600, 75, 1.0f));
+
+            // Build imagelist
+            this.ImageList.Add(typeof(EntityCreeper), Properties.Resources.creeper);
+            this.ImageList.Add(typeof(EntityExplode), Properties.Resources.tnt);
+            this.ImageList.Add(typeof(EntitySlower), Properties.Resources.spider);
+            this.ImageList.Add(typeof(EntityPlayer), Properties.Resources.player);
 
             // Initialize component
             InitializeComponent();
@@ -64,8 +75,37 @@ namespace Olympus_the_Game.View
             // Loop through all gameobjects
             foreach (GameObject go in objects)
             {
-                e.Graphics.FillRectangle(Brushes.Black, go.X * SCALE, go.Y * SCALE, BLOCK_SIZE, BLOCK_SIZE);
+                draw(go, e.Graphics);
             }
+
+            // Draw player
+            draw(pf.Player, e.Graphics);
+        }
+
+        private void draw(GameObject go, Graphics g)
+        {
+            // Select bitmap
+            Bitmap bm = null;
+            ImageList.TryGetValue(go.GetType(), out bm);
+            if (bm == null)
+                bm = Properties.Resources.rsz_arrowup; // TODO Change not implemented picture
+
+            // Generate target rectangle
+            Rectangle target = new Rectangle(
+                (int)((float)go.X * SCALE),
+                (int)((float)go.Y * SCALE),
+                (int)((float)go.Width * SCALE),
+                (int)((float)go.Height * SCALE));
+
+            //  Draw picture
+            g.DrawImage(bm,
+                target,
+                new Rectangle(0, 0, bm.Width, bm.Height),
+                System.Drawing.GraphicsUnit.Pixel);
+
+            // Add border
+            Pen p = new Pen(Brushes.Black);
+            g.DrawRectangle(p, target);
         }
     }
 }
