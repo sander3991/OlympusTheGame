cc4674fe934477565e762525f862512dcfb454f0
Martijn <martijnie96@gmail.com>
2014-09-29 10:34:03 +0200

    Basis layout LevelEditor begin aan gemaakt

---
+++ Olympus the Game/View/LevelEditor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Olympus_the_Game.View
+{
+    public partial class LevelEditor : Form
+    {
+        public LevelEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
