8253ab0fb6f257ce9d5acd93b0fa5e78e0fce80a
Martijn <martijnie96@gmail.com>
2014-09-29 11:58:18 +0200

    LevelEditor drag en drop events verder uitgewerkt

--- Olympus the Game/View/LevelEditor.Designer.cs
+++ Olympus the Game/View/LevelEditor.Designer.cs
@@ -52,6 +52,8 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(978, 540);
             this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
             // 
             // richTextBox1
             // 
