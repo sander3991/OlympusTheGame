f900b0ebff4bbfb73a0488268a8386211e719029
Martijn <martijnie96@gmail.com>
2014-09-30 12:50:17 +0200

    DetailEditor toegevoegd aan LevelEditor

---
+++ Olympus the Game/View/Editor/DetailEditor.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Olympus_the_Game.View.Editor
+{
+    partial class DetailEditor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DetailEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "DetailEditor";
+            this.Size = new System.Drawing.Size(844, 179);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
