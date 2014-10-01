e172f1c6041b1e394899387cd8dd5deed9f99c97
henkertjan <henkjanmulder@live.nl>
2014-10-01 10:14:43 +0200

    InfoView bewegen

--- Olympus the Game/View/InfoView.Designer.cs
+++ Olympus the Game/View/InfoView.Designer.cs
@@ -33,6 +33,7 @@
             this.PosX = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.PosY = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.EntitySpeed = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DragButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -42,9 +43,9 @@
             this.PosX,
             this.PosY,
             this.EntitySpeed});
-            this.listView1.Location = new System.Drawing.Point(-4, 1);
+            this.listView1.Location = new System.Drawing.Point(-8, -1);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(248, 502);
+            this.listView1.Size = new System.Drawing.Size(248, 436);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -65,13 +66,25 @@
             // 
             this.EntitySpeed.Text = "Speed";
             // 
+            // DragButton
+            // 
+            this.DragButton.Image = global::Olympus_the_Game.Properties.Resources.rsz_1dragbutton;
+            this.DragButton.Location = new System.Drawing.Point(200, 441);
+            this.DragButton.Name = "DragButton";
+            this.DragButton.Size = new System.Drawing.Size(40, 40);
+            this.DragButton.TabIndex = 3;
+            this.DragButton.UseVisualStyleBackColor = true;
+            this.DragButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragButton_MouseDown);
+            this.DragButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragButton_MouseMove);
+            // 
             // InfoView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DragButton);
             this.Controls.Add(this.listView1);
             this.Name = "InfoView";
-            this.Size = new System.Drawing.Size(240, 504);
+            this.Size = new System.Drawing.Size(240, 487);
             this.ResumeLayout(false);
 
         }
@@ -83,6 +96,7 @@
         private System.Windows.Forms.ColumnHeader PosX;
         private System.Windows.Forms.ColumnHeader PosY;
         private System.Windows.Forms.ColumnHeader EntitySpeed;
+        private System.Windows.Forms.Button DragButton;
 
     }
 }
