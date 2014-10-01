aa76554970061f4ce55e37df8438872c881b98ef
iElmar <elmarvdd@outlook.com>
2014-10-01 09:41:34 +0200

    Revert "Listview"
    This reverts commit a39d7f27618f4c275491b2fb0ae248ca78b72b89.

--- Olympus the Game/View/GameScreen.Designer.cs
+++ Olympus the Game/View/GameScreen.Designer.cs
@@ -33,11 +33,6 @@
             this.infoPanel1 = new Olympus_the_Game.View.InfoPanel();
             this.gamePanel1 = new Olympus_the_Game.View.GamePanel();
             this.customMenuBar1 = new Olympus_the_Game.View.MenuBar.CustomMenuBar();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.X = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Y = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Speed = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // arrowPanel1
@@ -52,7 +47,7 @@
             // infoPanel1
             // 
             this.infoPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.infoPanel1.Location = new System.Drawing.Point(517, 443);
+            this.infoPanel1.Location = new System.Drawing.Point(995, 41);
             this.infoPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.infoPanel1.Name = "infoPanel1";
             this.infoPanel1.Size = new System.Drawing.Size(258, 541);
@@ -75,44 +70,12 @@
             this.customMenuBar1.TabIndex = 4;
             this.customMenuBar1.Text = "customMenuBar1";
             // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Name,
-            this.X,
-            this.Y,
-            this.Speed});
-            this.listView1.Location = new System.Drawing.Point(996, 41);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(251, 489);
-            this.listView1.TabIndex = 5;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // Name
-            // 
-            this.Name.Text = "Naam";
-            // 
-            // X
-            // 
-            this.X.Text = "X";
-            // 
-            // Y
-            // 
-            this.Y.Text = "Y";
-            // 
-            // Speed
-            // 
-            this.Speed.Text = "Snelheid";
-            this.Speed.Width = 111;
-            // 
             // GameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Olympus_the_Game.Properties.Resources.dirt;
             this.ClientSize = new System.Drawing.Size(1264, 861);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.arrowPanel1);
             this.Controls.Add(this.gamePanel1);
             this.Controls.Add(this.infoPanel1);
@@ -137,10 +100,5 @@
         public GamePanel gamePanel1;
         private ArrowPanel arrowPanel1;
         private MenuBar.CustomMenuBar customMenuBar1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader Name;
-        private System.Windows.Forms.ColumnHeader X;
-        private System.Windows.Forms.ColumnHeader Y;
-        private System.Windows.Forms.ColumnHeader Speed;
     }
 }
