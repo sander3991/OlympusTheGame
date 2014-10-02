862e25ac925f15f2729739c7563a019f7e46eeee
Martijn <martijnie96@gmail.com>
2014-10-02 14:04:02 +0200

    Bug gefixt

--- Olympus the Game/Model/Entities/Entity.cs
+++ Olympus the Game/Model/Entities/Entity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Olympus_the_Game
+namespace Olympus_the_Game{
    public abstract class Entity : GameObject
     {
         /// <summary>
