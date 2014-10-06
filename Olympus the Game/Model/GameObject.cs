a062a7c25174411677922a8b5b6ed7041b403dcd
Sander van t Einde <sander_3991@live.nl>
2014-10-06 20:18:55 +0200

    Commentaar toegevoegd waar het miste

--- Olympus the Game/Model/GameObject.cs
+++ Olympus the Game/Model/GameObject.cs
@@ -229,7 +229,14 @@ namespace Olympus_the_Game
         {
 
         }
-        // TODO Sander: Comments
+       /// <summary>
+       /// Controleerd of 2 lijnen elkaar overlappen. Gebaseerd op hun startpositie en breedte (of Hoogte).
+       /// </summary>
+       /// <param name="x1">Beginpunt van lijn 1</param>
+       /// <param name="width1">Breedte van lijn 1</param>
+       /// <param name="x2">Beginpunt van lijn 2</param>
+       /// <param name="width2">Breedte van lijn 2</param>
+       /// <returns>True als ze elkaar overlappen, anders false</returns>
         public static bool DoLinesOverlap(int x1, int width1, int x2, int width2)
         {
             if (x1 >= x2)
