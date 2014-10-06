c5dc660e79b0cc2a5e58f1c46c0ca823c1e8f228
JoelJansenD <joeljansend@gmail.com>
2014-10-06 09:35:24 +0200

    MVS wilt dat ik commit

---
+++ Olympus the Game Test/Model/Entities/EntityCreeperTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Olympus_the_Game;
+using System;
+using System.Collections.Generic;
+
+namespace Olympus_the_Game_Test.Model.Entities {
+    [TestClass]
+    class EntityCreeperTests {
+        [TestMethod]
+        public void CreeperMovesToPlayer() 
+        {
+            Assert.IsTrue();
+        }
+    }
+}
