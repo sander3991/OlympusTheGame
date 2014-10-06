9bedde4e017a316e1fde6795af16e67933c228a7
Sander van t Einde <sander_3991@live.nl>
2014-10-06 14:48:20 +0200

    Extra constructor EntityFireball

--- Olympus the Game/Model/Sprites/SpriteExplosion.cs
+++ Olympus the Game/Model/Sprites/SpriteExplosion.cs
@@ -9,12 +9,14 @@ namespace Olympus_the_Game
     class SpriteExplosion : AnimatedSprite
     {
         //TODO Ruben : Entity standaard 1.5/2 keer zo groot maken
-        public SpriteExplosion(GameObject entity) : base(entity.Width, entity.Height, entity.X, entity.Y)
+        public SpriteExplosion(int width, int height, int x, int y) : base(width, height, x, y)
         {
             Type = ObjectType.SPRITEEXPLOSION;
             duration = 1000;
             OlympusTheGame.INSTANCE.Controller.UpdateGameEvents += OnUpdate;
         }
+
+        public SpriteExplosion(GameObject entity) : this(entity.Width, entity.Height, entity.X, entity.Y){ }
 
         public void OnUpdate()
         {
