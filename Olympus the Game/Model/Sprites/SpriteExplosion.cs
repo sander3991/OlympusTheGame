bd68fa632c152b4c238eb1a0bd16883e74dbae19
Sander van t Einde <sander_3991@live.nl>
2014-10-06 15:28:14 +0200

    Fix Merge

--- Olympus the Game/Model/Sprites/SpriteExplosion.cs
+++ Olympus the Game/Model/Sprites/SpriteExplosion.cs
@@ -16,13 +16,13 @@ namespace Olympus_the_Game
         /// Create a new SpriteExplosion, with the same location as the entity.
         /// </summary>
         /// <param name="entity"></param>
-        public SpriteExplosion(GameObject entity)
-            : base((int)((float)entity.Width * EXPLOSION_SCALE),
-            (int)((float)entity.Height * EXPLOSION_SCALE), 
-            entity.X - (int)((float)entity.Width * (EXPLOSION_SCALE - 1.0f)) / 2 , 
-            entity.Y - (int)((float)entity.Height * (EXPLOSION_SCALE - 1.0f)) / 2 )
+        public SpriteExplosion(GameObject entity) : this(entity.Width, entity.Height, entity.X, entity.Y) { }
+
         //TODO Ruben : Entity standaard 1.5/2 keer zo groot maken
-        public SpriteExplosion(int width, int height, int x, int y) : base(width, height, x, y)
+        public SpriteExplosion(int width, int height, int x, int y) : base((int)((float)width * EXPLOSION_SCALE),
+            (int)((float)height * EXPLOSION_SCALE), 
+            x - (int)((float)width * (EXPLOSION_SCALE - 1.0f)) / 2 , 
+            y - (int)((float)height * (EXPLOSION_SCALE - 1.0f)) / 2 )
         {
             Type = ObjectType.SPRITEEXPLOSION;
             duration = 1000;
@@ -32,8 +32,6 @@ namespace Olympus_the_Game
         /// <summary>
         /// Update this explosion.
         /// </summary>
-        public SpriteExplosion(GameObject entity) : this(entity.Width, entity.Height, entity.X, entity.Y){ }
-
         public void OnUpdate()
         {
             // If explosion is over, remove from PlayField.
