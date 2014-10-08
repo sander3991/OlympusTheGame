d2c62ffc201539b4593bac0ba38d835dedf97035
henkertjan <henkjanmulder@live.nl>
2014-10-08 11:49:31 +0200

    Extra functionaliteit voor de muziek spelers

--- Olympus the Game/Controller/CustomMusicPlayer.cs
+++ Olympus the Game/Controller/CustomMusicPlayer.cs
@@ -8,8 +8,13 @@ namespace Olympus_the_Game
 {
     public static class CustomMusicPlayer
     {
+        /// <summary>
+        /// Geef feedback over de player of hij aan het afspelen is
+        /// </summary>
         public static bool IsPlaying { get; private set; }
-
+        /// <summary>
+        /// Regel het volume
+        /// </summary>
         public static int MasterVolume
         {
             get
@@ -24,7 +29,10 @@ namespace Olympus_the_Game
 
         [DllImport("winmm.dll")]
         private static extern long mciSendString(string lpstrCommand, StringBuilder lpstrReturnString, int uReturnLength, int hwndCallback);
-
+        /// <summary>
+        /// Open een mp3 file
+        /// </summary>
+        /// <param name="file"></param>
         public static void Open(string file)
         {
             string command = "close MyMp3";
@@ -32,21 +40,32 @@ namespace Olympus_the_Game
             command = "open \"" + file + "\" type MPEGVideo alias MyMp3";
             mciSendString(command, null, 0, 0);
         }
-        public static void Play()
+        /// <summary>
+        /// Speel de file af
+        /// </summary>
+        /// <param name="repeat">Speel af met of zonder repeat</param>
+        public static void Play(bool repeat) 
         {
             string command = "play MyMp3";
+            if(repeat)
+                command += " REPEAT";
             mciSendString(command, null, 0, 0);
             IsPlaying = true;
             if (Mp3Player.IsPlaying)
                 Mp3Player.StopPlaying();
         }
+        /// <summary>
+        /// Pauzeer de speler.
+        /// </summary>
         public static void Pause()
         {
             string command = "pause MyMp3";
             mciSendString(command, null, 0, 0);
             IsPlaying = false;
         }
-
+        /// <summary>
+        /// Stop en gooi de mp3 weg
+        /// </summary>
         public static void Stop()
         {
             string command = "close MyMp3";
